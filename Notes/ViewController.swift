5b3ac5b05f29a7c01773712b2479f8ebfa2037a5
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-04-15 18:08:19 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Данил Толстиков on 15.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
