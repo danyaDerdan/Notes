3ceb7e213534d2bf45f8b458b609cafe31ddf816
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-04-16 09:51:15 +0300

    Added networkService

--- Notes/Services/NetworkService.swift
+++ Notes/Services/NetworkService.swift
@@ -1,7 +1,25 @@
-//
-//  NetworkService.swift
-//  Notes
-//
-//  Created by Данил Толстиков on 15.04.2025.
-//
+import Foundation
 
+protocol NetworkServiceProtocol {
+    func fetchData(from stringUrl: String, completion: @escaping (Result<ToDoResponse, Error>) -> Void)
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    
+    func fetchData(from stringUrl: String, completion: @escaping (Result<ToDoResponse, Error>) -> Void) {
+        guard let url = URL(string: stringUrl) else { completion(.failure(NetworkError.invalidURL)); return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error { completion(.failure(error)); return }
+            guard let data else { completion(.failure(NetworkError.invalidData)); return }
+            let decoder = JSONDecoder()
+            guard let result = try? decoder.decode(ToDoResponse.self, from: data) else { completion(.failure(NetworkError.decodingFailed)); return}
+            completion(.success(result))
+        }.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidData
+    case decodingFailed
+}
